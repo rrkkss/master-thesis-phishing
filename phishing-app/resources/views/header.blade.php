70d1d18526e164313924df06efa790ea9abe9b68
rrkkss <rockks@tutanota.com>
2024-03-17 03:27:24 +0100

    disallowed robots (those which comply anyways)

--- phishing-app/resources/views/header.blade.php
+++ phishing-app/resources/views/header.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name='robots' content='noindex,follow'/>
         <title>{{ \App\Helpers\AppHelper::getSiteName(request()->get('lang')) }}</title>
         <link rel="icon" href="{{ URL::asset('img/favicon.ico') }}">
         <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
--- phishing-app/resources/views/success.blade.php
+++ phishing-app/resources/views/success.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name='robots' content='noindex,follow' />
         <title>{{ \App\Helpers\AppHelper::getSiteName(request()->get('lang')) }}</title>
         <link rel="icon" href="{{ URL::asset('img/favicon.ico') }}">
         <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
@@ -31,7 +32,7 @@
                             <ul>
                                 <li>{{$translation->login}} <span class="phishing-data">{{$username}}</span> </li>
                                 <li>user agent - <span class="phishing-data">{{$useragent}}</span></li>
-                                <li>{{$translation->geo}} <span class="phishing-data">{{$geolocation}}</span></li>
+                                <li>{{$translation->geo}} <span class="phishing-data">{{$geolocation}}</span> (Hetzner hosting is blocked from using whois)</li>
                             </ul>
                         </li>
                     </ul>
